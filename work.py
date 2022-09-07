8b3b8564ad11056ef809851b22b49710262c2f73
hemanth5585 <59567835+hemanth5585@users.noreply.github.com>
2022-09-07 09:48:25 +0530

    update 2

--- work.py
+++ work.py
@@ -6,20 +6,28 @@ import pandas as pd
 import os
 excel = openpyxl.Workbook()
 print(excel.sheetnames)
-sheet = excel.active
+sheet = excel.active    
 sheet.title = "Chapters"
 sheet.append(['sub-topic'])
 print(excel)
 
 url = "https://en.wikipedia.org/wiki/React_(JavaScript_library)"
+base = "https://en.wikipedia.org"
 source = requests.get("https://en.wikipedia.org/wiki/React_(JavaScript_library)")
 source.raise_for_status()
 soup = BeautifulSoup(source.text,'html.parser')
 heading = soup.find("span",class_="mw-page-title-main").text
-print(heading)  
-for tag in soup.select('p a[href]'):
+print(heading)
+c = 0
+for tag in soup.select('p a[href]'):      
+    if c ==0:
+        sheet.append([heading])
+        c = 1
     topic = tag['href']
-    sheet.append([topic])
+    if topic.startswith('#') or topic.startswith('http'):
+        a = 0
+    else:
+        sheet.append([topic])
 file_name = "DataSet.xlsx"
 excel.save(file_name)
 
@@ -30,5 +38,6 @@ newPath = newPath + file_name
 
 df = pd.DataFrame(pd.read_excel(newPath))
 df = df.T
+df.filter(like ='#')
 print(df)
 df.to_excel('raw_data.xlsx')
