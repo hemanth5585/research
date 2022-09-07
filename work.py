3a7d1f98e57acff2c58f7afd7ff0c6c38be05e66
hemanth5585 <59567835+hemanth5585@users.noreply.github.com>
2022-09-07 08:03:22 +0530

    Updated

--- work.py
+++ work.py
@@ -13,6 +13,8 @@ url = "https://en.wikipedia.org/wiki/React_(JavaScript_library)"
 source = requests.get("https://en.wikipedia.org/wiki/React_(JavaScript_library)")
 source.raise_for_status()
 soup = BeautifulSoup(source.text,'html.parser')
+heading = soup.find("span",class_="mw-page-title-main").text
+print(heading)  
 for tag in soup.select('p a[href]'):
     topic = tag['href']
     sheet.append([topic])
