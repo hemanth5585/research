240ff8084c8ba817aaffb36d28c4c99d20998128
hemanth5585 <59567835+hemanth5585@users.noreply.github.com>
2022-09-10 20:07:18 +0530

    Add files via upload
    Extraction of links without spaces and indexes from data file

---
+++ data_operations.py
@@ -0,0 +1,51 @@
+
+from sys import path_hooks
+from bs4 import BeautifulSoup
+import requests, openpyxl,re,pathlib
+import pandas as pd
+import os
+excel = openpyxl.Workbook()
+print(excel.sheetnames)
+sheet = excel.active    
+sheet.title = "Chapters"
+print(excel)
+
+url = "https://en.wikipedia.org/wiki/React_(JavaScript_library)"
+base = "https://en.wikipedia.org"
+source = requests.get("https://en.wikipedia.org/wiki/React_(JavaScript_library)")
+source.raise_for_status()
+soup = BeautifulSoup(source.text,'html.parser')
+heading = soup.find("span",class_="mw-page-title-main").text
+print(heading)
+c = 0
+for tag in soup.select('p a[href]'):      
+    if c ==0:
+        sheet.append([heading])
+        c = 1
+    topic = tag['href']
+    if topic.startswith('#') or topic.startswith('http'):
+        a = 0
+    else:
+        sheet.append([topic])
+file_name = "DataSet.xlsx"
+excel.save(file_name)
+
+path_of_file = os.getcwd()
+newPath = path_of_file.replace(os.sep, '/')
+newPath = newPath + '/'
+newPath = newPath + file_name
+
+df = pd.DataFrame(pd.read_excel(newPath))
+
+df.filter(like ='#')
+#print(df)
+df.to_excel('raw_data.xlsx')
+df = df.T
+
+for row in (df.items()):
+    x = row[1]
+#print(type(x))
+df1 = x.to_frame().T
+df1["React (JavaScript library)"]
+f = df1.to_string(index=False).replace(" ","")
+print(f)
