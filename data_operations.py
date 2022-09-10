64faf911975da8f4ba9ac22dfa87cbe2812a020d
hemanth5585 <59567835+hemanth5585@users.noreply.github.com>
2022-09-10 20:18:41 +0530

    Add files via upload
    incl function

--- data_operations.py
+++ data_operations.py
@@ -1,5 +1,5 @@
-
 from sys import path_hooks
+from turtle import clear
 from bs4 import BeautifulSoup
 import requests, openpyxl,re,pathlib
 import pandas as pd
@@ -9,30 +9,39 @@ print(excel.sheetnames)
 sheet = excel.active    
 sheet.title = "Chapters"
 print(excel)
+def function(base,first,i):
+    i = str(i)
+    url = base+first
+    print(url)
+    source = requests.get(url)
+    source.raise_for_status()
+    soup = BeautifulSoup(source.text,'html.parser')
+    heading = soup.find("span",class_="mw-page-title-main").text
+    c = 0
+    for tag in soup.select('p a[href]'):      
+        if c ==0:
+            sheet.append([heading])
+            c = 1
+        topic = tag['href']
+        if topic.startswith('#') or topic.startswith('http'):
+            a = 0
+        else:
+            sheet.append([topic])
+    file_name = "Dataset"
+    file_name = file_name+i
+    extension = ".xlsx"
+    file_name = file_name+extension
+    print(file_name)
+    excel.save(file_name)
+    return file_name
 
-url = "https://en.wikipedia.org/wiki/React_(JavaScript_library)"
+
 base = "https://en.wikipedia.org"
-source = requests.get("https://en.wikipedia.org/wiki/React_(JavaScript_library)")
-source.raise_for_status()
-soup = BeautifulSoup(source.text,'html.parser')
-heading = soup.find("span",class_="mw-page-title-main").text
-print(heading)
-c = 0
-for tag in soup.select('p a[href]'):      
-    if c ==0:
-        sheet.append([heading])
-        c = 1
-    topic = tag['href']
-    if topic.startswith('#') or topic.startswith('http'):
-        a = 0
-    else:
-        sheet.append([topic])
-file_name = "DataSet.xlsx"
-excel.save(file_name)
-
 path_of_file = os.getcwd()
 newPath = path_of_file.replace(os.sep, '/')
 newPath = newPath + '/'
+first ="/wiki/React_(JavaScript_library)"
+file_name = function(base,first,0)
 newPath = newPath + file_name
 
 df = pd.DataFrame(pd.read_excel(newPath))
@@ -44,8 +53,8 @@ df = df.T
 
 for row in (df.items()):
     x = row[1]
+    df1 = x.to_frame().T
+    f = df1.to_string(index=False).replace(" ","")
+    print(f)
+    break
 #print(type(x))
-df1 = x.to_frame().T
-df1["React (JavaScript library)"]
-f = df1.to_string(index=False).replace(" ","")
-print(f)
