7106f78414faa27a8f499ba99e0faac1cd58d7e4
hemanth5585 <59567835+hemanth5585@users.noreply.github.com>
2022-09-10 22:55:36 +0530

    Working!!!!!!!
    Working!!!!!!!

--- work.py
+++ work.py
@@ -2,6 +2,9 @@ from sys import path_hooks
 from turtle import clear
 from types import new_class
 from bs4 import BeautifulSoup
+from operator import is_not
+from functools import partial
+import numpy as np
 import requests, openpyxl,re,pathlib
 import pandas as pd
 import os
@@ -20,7 +23,14 @@ def function(base,first,i):
     source = requests.get(url)
     source.raise_for_status()
     soup = BeautifulSoup(source.text,'html.parser')
-    heading = soup.find("span",class_="mw-page-title-main").text
+    
+    if soup.find("span",class_="mw-page-title-main"):
+        heading = soup.find("span",class_="mw-page-title-main")
+        print("Yes")
+    else:
+        return
+    heading = heading.text
+    print(type(heading))
     c = 0
     for tag in soup.select('p a[href]'):      
         if c ==0:
@@ -51,20 +61,36 @@ newPath = newPath + file_name
 print(newPath)
 df = pd.DataFrame(pd.read_excel(newPath))
 
-df.filter(like ='#')
+#df.filter(like ='#')
 #print(df)
 #df.to_excel('raw_data.xlsx')
 df = df.T
 i = 1
-
+paths = []
 for row in (df.items()):
-    x = row[1]
-    df1 = x.to_frame().T
-    f = df1.to_string(header=False,index=False).replace(" ","")
-    print(f)
-    print(base+f)
-    path = function(base,f,i)
-    print("**********Iteration done***********")
-    i=i+1
-    
-#print(type(x))
+    if i<3:
+        x = row[1]
+        df1 = x.to_frame().T
+        f = df1.to_string(header=False,index=False).replace(" ","")
+        path = function(base,f,i)
+        paths.append(path)
+    else:
+        break
+    i=i+1  
+
+paths = list(filter(None, paths))
+newPath = path_of_file.replace(os.sep, '/')
+i=i+1
+for j in range(len(paths)):
+    if i<100:
+        newPath = paths[j]
+        print(newPath)
+        df = pd.DataFrame(pd.read_excel(newPath))
+        df = df.T
+        for row in (df.items()):
+            x = row[1]
+            df1 = x.to_frame().T
+            f = df1.to_string(header=False,index=False).replace("c ","")
+            path = function(base,f,i)
+            paths.append(path)
+            i=i+1 
